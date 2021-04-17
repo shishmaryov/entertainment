216527b99b1e379a5d3b1b2a1906c0dbdc502930
Konstantin <shishmaryovkonstantin@gmail.com>
2021-04-17 22:31:21 +0300

    fix

--- build/js/change-password.js
+++ build/js/change-password.js
@@ -1,50 +1,16 @@
-/*#old-password
-#new-password
-#confirnm-password
+/*#oldPassword
+#newPassword
+#confirnmPassword
 */
 
-document.addEventListener('DOMContentLoaded', function () {
-  const form = document.getElementById('.change-password-page__inputs');
-  form.addEnetListener('submit', formSend);
-
-  async function formSend(e) {
-    e.preventDefault();
-
-    let error = formValidate(form);
-
-    if (error==0){
-
-    } else {
-      alert('Заполните обязательные поля');
+$('.changePasswordForm').validate({
+  rules: {
+    oldPassword: {
+      required: true
+    },
+    newPassword: {
+      required: true,
+      minlength: 6
     }
-  }
-
-  function formValidate(form) {
-    let error = 0;
-    let formReq = document.querySelectorAll('_req');
-
-    for (let index = 0; index < formReq.length; index++) {
-      const input = formReq[index];
-      formRemoveError(input);
-
-      if (input.classList.contains('change-password-page__input--old')) {
-        if (input.value === '') {
-          formAddError(input);
-          error++;
-          $('change-password-page__input--old').toggleClass('change-password-page__input--old-active');
-        }
-      }
-
-
-    }
-    return error;
-  }
-  function formAddError(input) {
-    input.parentElement.classList.add('_error');
-    input.classList.add('_error');
-  }
-  function formRemoveError(input) {
-    input.parentElement.classList.remove('_error');
-    input.classList.remove('_error');
   }
 });
